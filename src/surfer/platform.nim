817b971c8bb968890cf9a370197a675f814ea475
xTrayambak <xtrayambak@gmail.com>
2026-04-04 19:22:33 +0530

    fix: platform: initial conditionals

--- src/surfer/platform.nim
+++ src/surfer/platform.nim
@@ -2,7 +2,8 @@ type Platform* {.size: sizeof(uint8).} = enum
   Wayland = 0
 
 func getPlatform*(): Platform {.compileTime.} =
-  Platform.Wayland
+  when defined(linux) or defined(wayland):
+    return Platform.Wayland
 
 func usingPlatform*(platform: Platform): bool {.compileTime.} =
   getPlatform() == platform
